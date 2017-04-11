bab32179bc17d99c0250b366a3d0638017413953
Craig <p4061644@tees.ac.uk>
2017-04-11 16:46:33 +0100

    Small Changes

--- app/src/main/java/uk/ac/tees/p4072699/dogmapp/AddDogActivity.java
+++ app/src/main/java/uk/ac/tees/p4072699/dogmapp/AddDogActivity.java
@@ -9,7 +9,6 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class AddDogActivity extends AppCompatActivity {
-    Owner owner;
     DatabaseHandler dh = new DatabaseHandler(this);
 
     Owner owner;
