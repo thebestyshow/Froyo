b1cb536414da6c37f62fd183c4686a47021f50f2
thebestyshow <besty_2006@hotmail.co.uk>
2017-05-11 14:29:04 +0100

    making sure

--- app/src/main/java/uk/ac/tees/p4072699/dogmapp/StartWalk.java
+++ app/src/main/java/uk/ac/tees/p4072699/dogmapp/StartWalk.java
@@ -55,6 +55,7 @@ public class StartWalk extends AppCompatActivity {
                 } else {
                     selected.add(position);
 
+
                 }
             }
         });
---
+++ app/src/main/java/uk/ac/tees/p4072699/dogmapp/weatherdata/JSONPopulator.java
@@ -0,0 +1,8 @@
+package uk.ac.tees.p4072699.dogmapp.weatherdata;
+
+import org.json.JSONObject;
+
+public interface JSONPopulator {
+
+    void populate(JSONObject data);
+}
